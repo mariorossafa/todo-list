d5a7143d2408ee7504734c7828b1c1d0f8bd1a3c
mario <mariorossafa@gmail.com>
2017-07-23 23:56:31 -0300

    adicionando arquivos

---
+++ src/components/InputTask.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input class="new-todo"
+      @keyup.enter="addTask"
+      placeholder="O que precisa ser feito?">
+  </div>
+</template>
+
+<script>
+import { Task } from '../models/Task'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    addTask ($event) {
+      let value = $event.target.value
+      let task = new Task()
+      task.completed = false
+      task.title = value
+      this.$emit('newTask', task)
+      $event.target.value = ''
+      console.log('Adicionado')
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="todo-list">
+    <li v-for="todo in sortedTasks" class="todo">
+        <div class="view">
+            <label>{{ todo.title }}</label>
+        </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['todoList'],
+  computed: {
+    sortedTasks: function () {
+      let sorted = this.todoList
+      return sorted.sort(function (a, b) {
+        if (a.title < b.title) return -1
+        if (a.title > b.title) return 1
+        return 0
+      })
+    }
+  }
+}
+</script>
